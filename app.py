3e011a166c3a58cac8bf3aae3ba3e31ecefa75d7
Holger Ihrig <holger.ihrig@gmail.com>
2019-06-16 22:17:12 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,129 @@
+import dash
+import dash_table
+import dash_core_components as dcc
+import dash_html_components as html
+from datetime import datetime as dt
+import sbanken_data as sb
+import pandas as pd
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+
+def create_columns(df):
+    col = []
+    for i in df.columns:
+        if i in [
+            "accountingDate",
+            "amount",
+            "text",
+            "transactionType"
+        ]:
+            col.append({"name": i, "id": i, "hidden": False})
+        else:
+            col.append({"name": i, "id": i, "hidden": True})
+    return col
+
+
+def sanitize_transactions(transactions, key):
+    for entry in transactions:
+        entry.pop(key, None)
+    return transactions
+
+
+def get_standard_account_id(session, cId):
+    accounts = sb.get_accounts(session, cId)
+    for res in accounts:
+        if res["accountType"] == "Standard account":
+            return res["accountId"]
+
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div(children=[
+    html.H1('Sbanken Dashboard'),
+
+    dcc.DatePickerRange(
+        id="transaction-range",
+        min_date_allowed=dt(2012, 1, 1),
+        max_date_allowed=dt.now().date(),
+        start_date=dt(2019, 6, 1),
+        end_date=dt.now().date()
+    ),
+
+    html.Br(),
+
+    dcc.Checklist(
+        id="switches",
+        options=[
+            {"label": "Enable Sorting", "value": "sort"},
+            {"label": "Enable Filtering", "value": "filter"},
+        ],
+        values=["sort"],
+        labelStyle={"display": "inline-block"}
+    ),
+
+    html.Br(),
+
+    dash_table.DataTable(
+        id="table"
+    )
+])
+
+
+@app.callback(
+    [
+        dash.dependencies.Output("table", "columns"),
+        dash.dependencies.Output("table", "data"),
+    ],
+    [
+        dash.dependencies.Input("transaction-range", "start_date"),
+        dash.dependencies.Input("transaction-range", "end_date")
+    ]
+    )
+def update_table(start_date, end_date):
+    transactions = sb.get_transactions(http_session, customerId, accountId, start_date, end_date, fake=True)
+    transactions = sanitize_transactions(transactions, "cardDetails")
+
+    df = pd.DataFrame.from_dict(transactions, orient="columns")
+    col = create_columns(df)
+    return (
+        col,
+        df.to_dict("records"),
+    )
+
+
+@app.callback(
+    [
+        dash.dependencies.Output("table", "sorting"),
+        dash.dependencies.Output("table", "filtering")
+    ],
+    [
+        dash.dependencies.Input("switches", "values")
+    ]
+)
+def update_table_capabilities(values):
+    sorting = False
+    filtering = False
+
+    if "sort" in values:
+        sorting = True
+
+    if "filter" in values:
+        filtering = True
+
+    return (
+        sorting,
+        filtering
+    )
+
+
+if __name__ == '__main__':
+    import api_settings
+
+    http_session = sb.create_authenticated_http_session(api_settings.CLIENTID, api_settings.SECRET)
+
+    customerId = api_settings.CUSTOMERID
+
+    accountId = get_standard_account_id(http_session, customerId)
+
+    app.run_server(debug=True)
---
+++ sbanken_data.py
@@ -0,0 +1,88 @@
+from oauthlib.oauth2 import BackendApplicationClient
+import requests
+from requests_oauthlib import OAuth2Session
+import urllib.parse
+import json
+
+
+def create_authenticated_http_session(client_id, client_secret) -> requests.Session:
+    oauth2_client = BackendApplicationClient(client_id=urllib.parse.quote(client_id))
+    session = OAuth2Session(client=oauth2_client)
+    session.fetch_token(
+        token_url='https://auth.sbanken.no/identityserver/connect/token',
+        client_id=urllib.parse.quote(client_id),
+        client_secret=urllib.parse.quote(client_secret)
+    )
+    return session
+
+
+def get_customer_information(http_session: requests.Session, customerid):
+    response_object = http_session.get(
+        "https://api.sbanken.no/exec.customers/api/v1/Customers",
+        headers={'customerId': customerid}
+    )
+    print(response_object)
+    print(response_object.text)
+    response = response_object.json()
+
+    if not response["isError"]:
+        return response["item"]
+    else:
+        raise RuntimeError("{} {}".format(response["errorType"], response["errorMessage"]))
+
+
+def get_accounts(http_session: requests.Session, customerid):
+    response = http_session.get(
+        "https://api.sbanken.no/exec.bank/api/v1/Accounts",
+        headers={'customerId': customerid}
+    ).json()
+
+    if not response["isError"]:
+        return response["items"]
+    else:
+        raise RuntimeError("{} {}".format(response["errorType"], response["errorMessage"]))
+
+
+def get_transactions(http_session: requests.Session, customerid, accountid, startDate, endDate, fake=False):
+    if fake:
+        with open('mytransactions.json') as json_file:
+            data = json.load(json_file)
+        return data
+
+    rqbase = "https://api.sbanken.no/exec.bank/api/v1/Transactions/" + accountid
+    qstring = "?startDate=" + startDate + "&endDate=" + endDate
+
+    response = http_session.get(
+        rqbase + qstring,
+        headers={'customerId': customerid}
+    ).json()
+
+    if not response["isError"]:
+        return response["items"]
+    else:
+        raise RuntimeError("{} {}".format(response["errorType"], response["errorMessage"]))
+
+
+def categorize_transactions(transactions):
+    stransactions = {}
+    for t in transactions:
+        if t["transactionType"] not in stransactions:
+            stransactions[t["transactionType"]] = []
+        stransactions[t["transactionType"]].append(t)
+    return stransactions
+
+
+def sum_categories(stransactions):
+    sums = {}
+    for category, entry in stransactions.items():
+        sums[category] = 0
+        for item in entry:
+            sums[category] = sums[category] + item["amount"]
+    return sums
+
+
+def balance_total(csums):
+    balance = 0
+    for key, value in csums.items():
+        balance = balance + value
+    return balance
